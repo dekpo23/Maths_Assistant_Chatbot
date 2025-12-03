f9fe1603aff2861b373ab1d3e7bb65dc26aa7ff0
David Ekpo <jofesdavid@gmail.com>
2025-12-03 10:12:04 +0100

    cors

--- app.py
+++ app.py
@@ -3,12 +3,28 @@ from fastapi import FastAPI
 from chatbot import response, teaching
 from dotenv import load_dotenv
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+origins = ["http://localhost:8080"]
 
 
 app = FastAPI(version="1.0.0")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class Query(BaseModel):
     question: str
+
+class Count(BaseModel):
+    count: int
 
 @app.get("/")
 def home():
@@ -21,6 +37,8 @@ def post(requests: Query):
 @app.post("/teach")
 def post_teach(requests: Query):
     return {"Response:": teaching(requests.question)}
+
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host = "127.0.0.1", port=8080)
